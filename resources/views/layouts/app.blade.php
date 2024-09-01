28676115ba9521505b64fa23db8fff8883376281
UPT TIK Unsri <ka.tik@unsri.ac.id>
2024-09-01 20:54:53 +0700

    Update navbar brand text to include PT APO

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -41,7 +41,7 @@
         <nav class="navbar navbar-expand-md navbar-light bg-warning bg-gradient shadow-sm">
             <div class="container">
                 <a class="navbar-brand" href="{{ url('/') }}">
-                    <img src="{{asset('images/cgm.png')}}" alt="DS CGM" class="img-responsive" width=35"> <strong>DS CGM - PT APO</strong>
+                    <img src="{{asset('images/cgm.png')}}" alt="DS CGM" class="img-responsive" width=35"> <strong>DS CGM SAWIT - PT APO</strong>
                 </a>
                 <button class="navbar-toggler" type="button" data-bs-toggle="collapse"
                     data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent"
